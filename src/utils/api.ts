ed23a67b35db3b6d9bd58779d559587072b04897
Chandrashekar G <chandrashekarcsg97@gmail.com>
2024-04-25 18:13:01 -0400

    [FIX] Deleted env file

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,7 +1,6 @@
 import axios from 'axios';
 import config from '../../config.json';
 import { GoogleGenerativeAI } from "@google/generative-ai";
-import "dotenv/config";
 
 export const getProjects = async () => {
   const { data } = await axios.get(
