a5cbd228f3dfcccfad9f79ef807992797a3f6541
Chandrashekar G <chandrashekarcsg97@gmail.com>
2024-04-25 16:41:42 -0400

    [FIX] help command fix and exposed Google API fix

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -32,7 +32,7 @@ export const getQuote = async () => {
 
 export const getGemini = async (args: string[]): Promise<string> => {
   // For text-only input, use the gemini-pro model
-  const genAI = new GoogleGenerativeAI("AIzaSyDk7IC4g3sO4jaIP3JUqRdoGUn38wqjDNs");
+  const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
   const model = genAI.getGenerativeModel({ model: "gemini-pro"});
 
   const prompt = args.join(' ');
