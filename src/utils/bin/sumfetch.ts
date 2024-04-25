b3b201f9e0d9a74f0ecdd99583fab93fca2472b2
Chandrashekar G <chandrashekarcsg97@gmail.com>
2024-04-25 14:17:30 -0400

    Update text art 2

--- src/utils/bin/sumfetch.ts
+++ src/utils/bin/sumfetch.ts
@@ -3,24 +3,35 @@ import config from '../../../config.json';
 const sumfetch = async (args: string[]): Promise<string> => {
   if (config.ascii === 'chand') {
     return `                                                     
-         　　██░▀██████████████▀░██          sumfetch: summary
-         　　█▌▒▒░████████████░▒▒▐█         -----------
-         　　█░▒▒▒░██████████░▒▒▒░█
-         　　▌░▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒░▐          ABOUT
-         　　░▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒░          ${config.name}
-         　 ███▀▀▀██▄▒▒▒▒▒▒▒▄██▀▀▀██        ﰩ ${config.ps1_hostname}
-         　 ██░░░▐█░▀█▒▒▒▒▒█▀░█▌░░░█         <u><a href="${config.resume_url}" target="_blank">Resume</a></u>
-         　 ▐▌░░░▐▄▌░▐▌▒▒▒▐▌░▐▄▌░░▐▌        爵 <u><a href="https://github.com/${config.social.github}?tab=repositories" target="_blank">Projects</a></u>
-         　　█░░░▐█▌░░▌▒▒▒▐░░▐█▌░░█         -----------
-         　　▒▀▄▄▄█▄▄▄▌░▄░▐▄▄▄█▄▄▀▒
-         　　░░░░░░░░░░└┴┘░░░░░░░░░          CONTACT
-         　　██▄▄░░░░░░░░░░░░░░▄▄██          <u><a href="mailto:${config.email}" target="_blank">${config.email}</a></u>
-         　　████████▒▒▒▒▒▒████████          <u><a href="https://github.com/${config.social.github}" target="_blank">github.com/${config.social.github}</a></u>
-         　　█▀░░███▒▒░░▒░░▒▀██████          <u><a href="https://linkedin.com/in/${config.social.linkedin}" target="_blank">linkedin.com/in/${config.social.linkedin}</a></u>
-         　　█▒░███▒▒╖░░╥░░╓▒▐█████
-         　　█▒░▀▀▀░░║░░║░░║░░█████
-         　　██▄▄▄▄▀▀┴┴╚╧╧╝╧╧╝┴┴███
-         　　██████████████████████
+  ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓         sumfetch: summary
+  ▓▓░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░▓▓        -----------
+  ▓▓░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░▓▓
+  ▓▓░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░▓▓         ABOUT
+  ▓▓░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░▓▓         ${config.name}
+  ▓▓░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░▓▓        ﰩ ${config.ps1_hostname}
+  ▓▓░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░▓▓         <u><a href="${config.resume_url}" target="_blank">Resume</a></u>
+  ▓▓░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░▓▓        爵 <u><a href="https://github.com/${config.social.github}?tab=repositories" target="_blank">Projects</a></u>
+  ▓▓░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░▓▓        -----------
+  ▓▓░░░░░▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓░░░░░░░░░░░░░░░▓▓
+  ▓▓░░░░░▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓░░░░░░░░░░░░░░░▓▓         CONTACT
+  ▓▓░░░░░▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓░░░░░░░░░░░░░░░▓▓         <u><a href="mailto:${config.email}" target="_blank">${config.email}</a></u>
+  ▓▓░░░░░▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓░░░░░░░░░░░░░░░▓▓         <u><a href="https://github.com/${config.social.github}" target="_blank">github.com/${config.social.github}</a></u>
+  ▓▓░░░░░chand$            ░░░░░░░░░░░░░░░▓▓         <u><a href="https://linkedin.com/in/${config.social.linkedin}" target="_blank">linkedin.com/in/${config.social.linkedin}</a></u>
+  ▓▓░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░▓▓
+  ▓▓░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░▓▓
+  ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓
+  ____▒__________▒▒▒▒▒▒▒▒▒▒▒▒▒▒
+  __▒_______________▒▒▒▒▒▒▒▒
+  _▒________________▒▒▒▒▒▒▒▒
+  _▒___________▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒
+__▒
+__▒______▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓
+_▒______▓▒▓▒▓▒▓▒▓▒▓▒▓▒▓▒▓▒▓▒▓▒▓▒▓▒▓▒▓▒▓▒▓▒▓
+▒▒▒▒___▓▒▓▒▓▒▓▒▓▒▓▒▓▒▓▒▓▒▓▒▓▒▓▒▓▒▓▒▓▒▓▒▓▒▓
+▒▒▒▒__▓▒▓▒▓▒▓▒▓▒▓▒▓▒▓▒▓▒▓▒▓▒▓▒▓▒▓▒▓▒▓▒▓▒▓
+▒▒▒__▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓
+▒▒
+
 
 `;
   } else {
